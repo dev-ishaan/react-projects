b36dcf17be86497446ef1cead763079d8c7cbb98
Ishaan Singh <ishaan.singh2k@gmail.com>
2024-06-07 21:00:44 +0530

    Using env variables for API key

--- newsapp-project/src/components/News.js
+++ newsapp-project/src/components/News.js
@@ -35,7 +35,7 @@ export class News extends Component {
 
     async componentDidMount(){
         this.props.setProgress(10)
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dde03b8c705e421da41e78ea7bd0e32c&page=${this.state.page}&pageSize=${this.props.pageSize}`
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`
         this.setState({loading: true})
         let data = await fetch(url);
         this.props.setProgress(40)
@@ -51,7 +51,7 @@ export class News extends Component {
 
     fetchMoreData = async ()=>{
         this.setState({page: this.state.page +1})
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dde03b8c705e421da41e78ea7bd0e32c&page=${this.state.page}&pageSize=${this.props.pageSize}`
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
