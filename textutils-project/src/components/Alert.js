c19c8682381322e5608710edf9c8f7a81d265805
Ishaan Singh <ishaan.singh2k@gmail.com>
2024-07-04 00:22:24 +0530

    Fixing first alphabet to be capital in alert

--- textutils-project/src/components/Alert.js
+++ textutils-project/src/components/Alert.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 export default function Alert(props) {
-    // const capital =(word)=>{
-    //     const lower = word?.toLowercase();
-    //     return lower.charAt(0).toUpperCase() + lower.slice(1);
-    // }
+    const capital =(word)=>{
+        const lower = word?.toLowerCase();
+        return lower.charAt(0).toUpperCase() + lower.slice(1);
+    }
   return (
     <div style={{height: '15px'}}>
     {props.alert && <div>
          <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-            <strong>{(props.alert.type)}</strong>: {props.alert.msg}
+            <strong>{capital(props.alert.type)}</strong>: {props.alert.msg}
         </div>
     </div>}
     </div>
