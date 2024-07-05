e958e123f8f8f369454d971dd2581cbc493e60d1
Ishaan Singh <ishaan.singh2k@gmail.com>
2024-07-06 01:34:57 +0530

    Creating Shopify app to buy items

---
+++ redux-app/src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h5>Buy Nike Joggers 2000 Rs</h5>
+      <button className="btn btn-primary mx-2">-</button>
+      Add this item to card
+      <button className="btn btn-primary mx-2">+</button>
+    </div>
+  )
+}
+
+export default Shop
