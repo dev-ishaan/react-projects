d88f196d47e22844b90682fb5ac3dee1b4ebca5c
Ishaan Singh <ishaan.singh2k@gmail.com>
2024-07-08 23:20:09 +0530

    Updating balance without using bindActionCreators from redux

--- redux-app/src/components/Shop.js
+++ redux-app/src/components/Shop.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+// import { bindActionCreators } from 'redux'
+import { actionCreators } from '../state/index'
 
 const Shop = () => {
+  const dispatch = useDispatch()
+  // const { increment, decrement } = bindActionCreators(actionCreators, dispatch)
   return (
     <div>
         <h5>Deposit/Withdraw Money</h5>
-      <button className="btn btn-primary mx-2">-</button>
+      <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.withdrawMoney(100))}}>-</button>
       Update the Balance
-      <button className="btn btn-primary mx-2">+</button>
+      <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreators.depositMoney(100))}}>+</button>
     </div>
   )
 }
